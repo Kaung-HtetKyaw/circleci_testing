6406b98113631f585040c6adf14f7d667e7183e3
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2022-03-21 01:39:04 +0630

    fix: change title

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -12,9 +12,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Continuous Integration with CircleCI</p>
         <a
           className="App-link"
           href="https://reactjs.org"

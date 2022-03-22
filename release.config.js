5a65cb14b89c7d76a114f45916f2d954aaec7a33
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2022-03-22 22:50:53 +0630

    fix: fix wrong eslint config

--- release.config.js
+++ release.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  branches: [
-    '+([0-9])?(.{+([0-9]),x}).x',
-    {
-      name: 'stable',
-      channel: false,
-    },
-    {
-      name: 'main',
-      channel: 'next',
-      prerelease: 'next',
-    },
-  ],
+  release: {
+    branches: [
+      '+([0-9])?(.{+([0-9]),x}).x',
+      {
+        name: 'stable',
+        channel: false,
+      },
+      {
+        name: 'main',
+        channel: 'next',
+        prerelease: 'next',
+      },
+    ],
+  },
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',

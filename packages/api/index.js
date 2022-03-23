7713d36f75b58633df3ec93990ab9cabeef42da9
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2022-03-23 18:35:03 +0630

    fix: dummy change

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 const app = express();
+const port = 3001;
 
 app.get('/', (_, res) => {
   res.status(200).send('Hello World');
 });
 
-app.listen(3001, () => {
-  console.log('Server is running at 3001');
+app.listen(port, () => {
+  console.log(`Server is running at localhost:${port}`);
 });

e7e55925e49862dde3b42bad531a3aa2c61c654b
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2022-03-23 17:33:53 +0630

    feat(ct-10): add notify plugin and linting commit

--- release.config.js
+++ release.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  branches: ['main'],
+  branches: ['main', 'stable', 'next'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@circleci_testing/repo-node-deploy/deploy-api.js',
     '@circleci_testing/repo-node-deploy/deploy-frontend.js',
-    ['@semantic-release/github', { success: false, fail: false }],
+    '@circleci_testing/repo-node-deploy/notify.js',
+    '@semantic-release/github',
   ],
 };
